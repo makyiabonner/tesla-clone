b623aa2e73cac783be25641a753beac197f94da7
makyia bonner <makyiabonner@gmail.com>
2023-08-01 12:53:07 -0400

    credit-info area done

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,13 +1,13 @@
-import styles from 'nav.module.scss';
+import styles from './nav.module.scss';
 
 
 export default function Nav(){
     return (
         <>
             <div className={styles.credit_info}>
-                <h3 className={styles.credit_info_h3}>'17,500 Federal Tax Credit'</h3>
-                <p className={styles.credit_info_p}>'Available for new Model 3 and Model Y. Reductions to tax credit likely after Dec 31.'</p>
-                <a href="/" className={styles.credit_info_a}>'See Details'</a>
+                <h3 className={styles.credit_info_h3}>$7,500 Federal Tax Credit</h3>
+                <p className={styles.credit_info_p}>Available for new Model 3 and Model Y. Reductions to tax credit likely after Dec 31.</p>
+                <a href="/" className={styles.credit_info_a}>See Details</a>
             </div>
             <nav className={styles.nav}>
                 <img src="" alt="tesla-icon" className={styles.logo} />
